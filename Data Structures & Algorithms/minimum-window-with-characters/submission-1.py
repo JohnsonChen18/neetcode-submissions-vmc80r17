91288e5ba9b504e517820ad692e86077fa6a110d
JohnsonChen18 <154767759+JohnsonChen18@users.noreply.github.com>
2026-04-16 21:18:09 -0400

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        list1 = [0] * 58
+        list2 = [0] * 58
+        curr_match = 0
+        match_goal = 0
+        left = 0
+        right = 0
+        min_len = 2**31-1
+        start = -1
+        end = -1
+
+        for char in t:
+            list2[ord(char) - ord('A')] += 1
+
+        while right < len(s):
+            while right < len(s) and self.ifContains(list1, list2) == False:
+                list1[ord(s[right]) - ord('A')] += 1
+                right += 1
+            right -= 1
+            while left <= right and self.ifContains(list1, list2) == True:
+                if (right - left + 1) < min_len:
+                    min_len = right - left + 1
+                    start = left
+                    end = right
+                list1[ord(s[left]) - ord('A')] -= 1
+                left += 1
+            right += 1
+        
+        return s[start:end+1]
+    
+    def ifContains(self, list1, list2):
+        for i in range(58):
+            if list2[i] == 0: continue
+            if list1[i] >= list2[i]:continue
+            return False
+        return True
