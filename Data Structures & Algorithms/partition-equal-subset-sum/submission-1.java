20fdc1f25b1879c9fbf02ef05587a419006083a3
JohnsonChen18 <154767759+JohnsonChen18@users.noreply.github.com>
2026-08-18 00:08:23 -0400

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        int target = sum / 2;
+        int n = nums.length;
+
+        // dp[i][j]:
+        // 使用前 i 个数字，能不能凑出 sum = j
+        boolean[][] dp = new boolean[n + 1][target + 1];
+
+        // 凑出 0 永远可以：什么都不选
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int curr = nums[i - 1];
+
+            for (int j = 1; j <= target; j++) {
+
+                // 选择 1：不用当前数字
+                boolean withCurr = dp[i - 1][j];
+
+                // 选择 2：使用当前数字
+                boolean withoutCurr = (j - curr >= 0)? dp[i - 1][j - curr]: false;
+                
+                dp[i][j]= withCurr || withoutCurr;
+            }
+        }
+
+        return dp[n][target];
+    }
+}
